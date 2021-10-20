2e43cdaff34b206255fbc903b731b6d6b33c0b88
vmonoge <vmonoge@gmail.com>
2021-10-20 23:30:18 +0200

    second challenge changes commit

--- task-07-celsius-to-fahrenheit.py
+++ task-07-celsius-to-fahrenheit.py
@@ -1,1 +1,6 @@
-def
+def celsius_to_fahrenheit(celsius_temperature):
+	fahrenheit_temperature = celsius_temperature
+
+	return print(f"{celsius_temperature} degrees Celsius: {fahrenheit_temperature} degrees Fahrenheit")
+
+celsius_to_fahrenheit(23)
--- task-07-fahrenheit-to-celsius.py
+++ task-07-fahrenheit-to-celsius.py
@@ -1,1 +1,6 @@
-def
+def fahrenheit_to_celcius(fahrenheit_temperature):
+	celcius_temperature = fahrenheit_temperature
+
+	return print(f"{fahrenheit_temperature} degrees Fahrenheit: {celcius_temperature} degrees Celcius")
+
+fahrenheit_to_celcius(23)
