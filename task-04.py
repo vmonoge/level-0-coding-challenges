dde7eb07153d0dd0e402de8b21c6ac9e6735def9
Victor Monoge <47704578+vmonoge@users.noreply.github.com>
2021-10-23 00:26:30 +0200

    Update task-04.py
    Four space indentation changes.

--- task-04.py
+++ task-04.py
@@ -1,8 +1,8 @@
 def even_or_odd(integer_input):
-	if integer_input % 2 == 0:
-		return print(f"{integer_input} is even")
-	else:
-		return print(f"{integer_input} is odd")
+    if integer_input % 2 == 0:
+        return print(f"{integer_input} is even")
+    else:
+        return print(f"{integer_input} is odd")
 
 even_or_odd(3)
 even_or_odd(4) 
