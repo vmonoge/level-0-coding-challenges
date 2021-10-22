920a75900b174aec798aec93d9a5e9310dffd83d
Victor Monoge <47704578+vmonoge@users.noreply.github.com>
2021-10-23 00:28:33 +0200

    Update task-05.py
    Four space indentation changes.

--- task-05.py
+++ task-05.py
@@ -1,15 +1,15 @@
 def area_of_a_triangle(side_one, side_two, side_three):
-	semi_parameter = (side_one + side_two + side_three) /2
+    semi_parameter = (side_one + side_two + side_three) /2
 
-	# reassigned to reduce var name length
-	s = semi_parameter
-	a = side_one
-	b = side_two
-	c = side_three
+    # reassigned to reduce var name length
+    s = semi_parameter
+    a = side_one
+    b = side_two
+    c = side_three
 
-	# Heron's Formula for triangle area calculation
-	area =  (s * (s - a) * (s - b) * (s - c)) ** (1/2)
+    # Heron's Formula for triangle area calculation
+    area =  (s * (s - a) * (s - b) * (s - c)) ** (1/2)
 
-	return print(f"Area of your triangle: {area} sqaure units")
+    return print(f"Area of your triangle: {area} sqaure units")
 
 area_of_a_triangle(3, 6, 7)
