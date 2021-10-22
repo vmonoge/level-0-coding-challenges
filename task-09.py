d37e017bba8ea5b09740ebbc99b8598f802787f0
Victor Monoge <47704578+vmonoge@users.noreply.github.com>
2021-10-23 00:34:48 +0200

    Update task-09.py
    Four space indentation changes.

--- task-09.py
+++ task-09.py
@@ -1,11 +1,11 @@
 def vowels_in_a_word(given_word):
-	vowel_list = ["a", "e", "i", "o", "u"]
-	given_word = given_word.lower()
-	found_vowels = ""
+    vowel_list = ["a", "e", "i", "o", "u"]
+    given_word = given_word.lower()
+    found_vowels = ""
 
-	for letter in given_word:
-		if letter in vowel_list and letter not in found_vowels:
-			found_vowels += f"{letter}, "
+    for letter in given_word:
+        if letter in vowel_list and letter not in found_vowels:
+            found_vowels += f"{letter}, "
 
 	return print(f"Vowels: {found_vowels[:-2]}.")
 
