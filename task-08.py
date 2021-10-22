4d0d4189f72e7d1b063c7e7032d7ee349f0468da
Victor Monoge <47704578+vmonoge@users.noreply.github.com>
2021-10-23 00:33:43 +0200

    Update task-08.py
    Four space indentation changes.

--- task-08.py
+++ task-08.py
@@ -1,18 +1,18 @@
 def number_to_time(number):
-	hours = number // 60
-	minutes = number - hours * 60
+    hours = number // 60
+    minutes = number - hours * 60
 
-	if hours == 0 or hours > 1:
-		 hour_name = f"{hours} hours"
-	else:
-		 hour_name = f"{hours} hour"
+    if hours == 0 or hours > 1:
+        hour_name = f"{hours} hours"
+    else:
+	hour_name = f"{hours} hour"
 
-	if minutes == 0 or minutes > 1:
-		 minute_name = f"{minutes} minutes"
-	else:
-		 minute_name = f"{minutes} minute"
+    if minutes == 0 or minutes > 1:
+        minute_name = f"{minutes} minutes"
+    else:
+        minute_name = f"{minutes} minute"
 
-	return print(f"{hour_name}, {minute_name}")
+    return print(f"{hour_name}, {minute_name}")
 
 number_to_time(0)
 number_to_time(60)
