edfcf3ccb2bfdd47c6cb0a5478f951b252d3012e
Victor Monoge <47704578+vmonoge@users.noreply.github.com>
2021-10-23 00:30:37 +0200

    Update task-06.py
    Four space indentation changes.

--- task-06.py
+++ task-06.py
@@ -1,10 +1,10 @@
 def maximum_number(first_number, second_number, third_number):
-	if first_number >= second_number and first_number >= third_number :
-		return print(first_number)
-	elif second_number >= first_number and second_number >= third_number:
-		return print(second_number)
-	else:
-		return print(third_number)
+    if first_number >= second_number and first_number >= third_number :
+        return print(first_number)
+    elif second_number >= first_number and second_number >= third_number:
+        return print(second_number)
+    else:
+        return print(third_number)
 
 maximum_number(1, 2, 3)
 maximum_number(3, 2, 1)
