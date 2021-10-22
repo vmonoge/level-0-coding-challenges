69dc8cb09e87ce23f369533554d431d49f4120ad
Victor Monoge <47704578+vmonoge@users.noreply.github.com>
2021-10-23 00:18:31 +0200

    Delete task-07-fahrenheit-to-celsius.py

--- task-07-fahrenheit-to-celsius.py
+++
@@ -1,6 +0,0 @@
-def fahrenheit_to_celcius(fahrenheit_temperature):
-	celcius_temperature = (fahrenheit_temperature - 32) * (5/9)
-
-	return print(f"{fahrenheit_temperature} degrees Fahrenheit: {celcius_temperature} degrees Celcius")
-
-fahrenheit_to_celcius(23)
