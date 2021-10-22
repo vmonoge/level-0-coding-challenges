8e7dafdcb5025b5a1ace4c76eac1db20c1b47246
Victor Monoge <47704578+vmonoge@users.noreply.github.com>
2021-10-23 00:20:10 +0200

    Update and rename task-07-celsius-to-fahrenheit.py to task-07.py
    Combining two functions in one file.

--- task-07-celsius-to-fahrenheit.py
+++
@@ -1,6 +0,0 @@
-def celsius_to_fahrenheit(celsius_temperature):
-	fahrenheit_temperature = celsius_temperature * (9/5) + 32
-
-	return print(f"{celsius_temperature} degrees Celsius: {fahrenheit_temperature} degrees Fahrenheit")
-
-celsius_to_fahrenheit(23)
---
+++ task-07.py
@@ -0,0 +1,14 @@
+def celsius_to_fahrenheit(celsius_temperature):
+	fahrenheit_temperature = celsius_temperature * (9/5) + 32
+
+	return print(f"{celsius_temperature} degrees Celsius: {fahrenheit_temperature} degrees Fahrenheit")
+
+celsius_to_fahrenheit(23)
+
+
+def fahrenheit_to_celcius(fahrenheit_temperature):
+	celcius_temperature = (fahrenheit_temperature - 32) * (5/9)
+
+	return print(f"{fahrenheit_temperature} degrees Fahrenheit: {celcius_temperature} degrees Celcius")
+
+fahrenheit_to_celcius(23)
