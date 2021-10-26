cc6994528c67ff3e17ca78ba9c96da94cb205eb0
Victor Monoge <47704578+vmonoge@users.noreply.github.com>
2021-10-26 17:04:33 +0200

    Update task-06.py
    Storage variable references the first item and test cases are printed instead of printing in the return statement. The bonus challenge is part of task 06.

--- task-06.py
+++ task-06.py
@@ -1,12 +1,13 @@
 def maximum_number(*args):
-    number_storage = 0
+    number_storage = args[0]
     
     for number in args:
-        if number >= number_storage:
-            number_storage = number
-    return print(number_storage)
 
-maximum_number(1, 2, 3)
-maximum_number(3, 2, 1)
-maximum_number(2, 1, 3)
-maximum_number(3, 1, 2)
+        if number >= number_storage :
+            number_storage = number
+    return number_storage
+
+print(maximum_number(-1, -2, -3))
+print(maximum_number(3, 2, 1))
+print(maximum_number(2, 1, 3))
+print(maximum_number(3, 1, 2))
